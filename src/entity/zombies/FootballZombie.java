085859e1823a5d64e7b793a9146cc639ccfaa5bc
sseekkaarr <sekarjenar.sj@gmail.com>
2024-05-05 17:05:01 +0700

    update ducky tube, football, imp, normal zombie

--- src/entity/zombies/FootballZombie.java
+++ src/entity/zombies/FootballZombie.java
@@ -1,5 +1,8 @@
 package entity.zombies;
 
-public class FootballZombie {
+public class FootballZombie extends Zombie {
     
+    public FootballZombie(){
+        super("Football Zombie", 360,100,1,false,100);
+    }
 }
--- src/entity/zombies/ImpZombie.java
+++ src/entity/zombies/ImpZombie.java
@@ -1,7 +1,9 @@
 package entity.zombies;
 
 public class ImpZombie extends Zombie {
+
     public ImpZombie(){
-        super(name:"Imp Zombie", health:90, attack_damage:80, attack_speed:1, is_aquatic:false);
+
+        super("Imp Zombie", 90, 80, 1, false, 150);
     }
 }
