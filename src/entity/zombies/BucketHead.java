e4060c94bf9f79562b7177639486a22cf37cf318
AlfazaNaufalZakiy <118534903+AlfazaNaufalZakiy@users.noreply.github.com>
2024-05-06 16:21:10 +0700

    add bucket,conehead, gargantuar, polevaulting zombie

---
+++ src/entity/zombies/BucketHead.java
@@ -0,0 +1,8 @@
+package entity.zombies;
+
+public class BucketHead extends Zombie {
+
+    public BucketHead(){
+        super("BucketHead Zombie",300,100,1,false,100);
+    }
+}
---
+++ src/entity/zombies/ConeHead.java
@@ -0,0 +1,8 @@
+package entity.zombies;
+
+public class ConeHead extends Zombie {
+    
+    public ConeHead(){
+        super("Conehead Zombie",250,100,1,false,100);
+    }
+}
